6466a56da33bb862d2320242c4a64d5ad6942c3d
Ray <771446555@qq.com>
2024-03-12 00:44:52 +0800

    refactor: change image layout mode

--- app/pin/[pinId]/components/PinImage.tsx
+++ app/pin/[pinId]/components/PinImage.tsx
@@ -3,68 +3,63 @@
 import { LuArrowUpRight } from "react-icons/lu"
 import Button from "@/components/shared/Button"
 import Image from "next/image"
-import { useEffect, useRef, useState } from "react"
+import { useLayoutEffect, useState } from "react"
 
-export default function PinImage({ src }: { src: string }) {
+interface Props {
+  src: string
+  width: number
+  height: number
+}
+export default function PinImage({ src, width, height }: Props) {
   /*
     The displayed width of the image should not exceed 508. If it does, the size of 
-      the image should be minified while the aspect-ratio should be remained.
+      the image should be minified while the aspect-ratio remains the same.
   */
-  const [imageSize, setimageSize] = useState({ width: 0, height: 0 })
-  const [isBigImage, setIsBigImage] = useState(false)
-  const imgContainerRef = useRef<HTMLDivElement>(null)
 
-  useEffect(() => {
-    const newImg = document.createElement("img")
-    newImg.src = src
-    newImg.onload = (ele: Event) => {
-      let { width, height } = ele.target as HTMLImageElement
-      const ratio = width / height
-
-      // mobile devices
-      if (window.innerWidth < 540) {
-        width = window.innerWidth
-        height = width / ratio
-        height = Math.min(window.innerHeight, height)
-        setIsBigImage(true)
-      }
-      // other devices
-      else if (width < 508 || height < 592) {
-        width = Math.min(width, 508 - 40) // paddings on 4 sides are 20px
-        height = width / ratio
-        setIsBigImage(false)
-      } else if (width >= 508) {
-        width = 508
-        height = width / ratio
-        setIsBigImage(true)
-      }
-      setimageSize({ width, height })
+  const [isBigImage, setIsBigImage] = useState(true)
+  const [displaySize, setDisplaySize] = useState({ width, height })
+  const ratio = width / height
+  useLayoutEffect(() => {
+    const size = { width: 0, height: 0 }
+    // mobile devices
+    if (window.innerWidth < 820) {
+      size.width = window.innerWidth
+      size.height = Math.min(window.innerHeight, size.width / ratio)
+      setIsBigImage(true)
     }
+    // other devices
+    else if (width < 508 || height < 592) {
+      size.width = Math.min(width, 508 - 40) // paddings on 4 sides are 20px
+      size.height = size.width / ratio
+      setIsBigImage(false)
+    } else if (width >= 508) {
+      size.width = 508
+      size.height = size.width / ratio
+      setIsBigImage(true)
+    }
+    setDisplaySize(size)
   }, [])
 
-  useEffect(() => {
-    const imgContainer = imgContainerRef.current
-    if (imageSize.width == 0 || !imgContainer) return
-
-    imgContainer.style.width = `${imageSize.width}px`
-    imgContainer.style.height = `${imageSize.height}px`
-  }, [imageSize])
-
   return (
-    <div className={`flex items-center justify-center box-border max-h-[902px] ${isBigImage ? "" : "p-5"}`}>
-      {/* if the width of the image is less than 508 or the height is less than 592,
-            the image should float in the center of the container and its border-radius
-            should be 16px
-        */}
-      <div ref={imgContainerRef} className="relative hover-visible-container">
+    <div
+      className={`pinImage flex items-center justify-center box-border pin-image-width max-h-screen w5:min-h-[592px] w5:max-h-[902px] overflow-hidden w3:max-w5:rounded-t-[2rem] w5:rounded-l-[2rem] ${
+        isBigImage ? "" : "p-5"
+      }`}>
+      {/*
+          if the width of the image is less than 508 or the height is less than 592,
+        the image should float in the center of the container and its border-radius
+        should be 16px
+      */}
+      <div className="relative hover-visible-container w5:max-h-[902px] overflow-hidden">
         <Image
-          className={`object-cover ${isBigImage ? "" : "rounded-2xl"}`}
+          className={`object-cover ${isBigImage ? "" : "rounded-2xl"} bg-gray-bg-4`}
           src={src}
           alt="pin image"
-          fill
+          width={displaySize.width}
+          height={displaySize.height}
           quality={100}
           priority={true}
-          sizes="(max-width: 540px) 100vw, (min-width: 540px) 508px"
+          sizes="(max-width: 820px) 100vw, (min-width: 820px) 508px"
         />
         <div className="absolute bottom-4 px-5 flex w-full justify-between">
           <a href={src} target="_blank" className="hover-content-visible">
@@ -75,9 +70,6 @@ export default function PinImage({ src }: { src: string }) {
               </div>
             </Button>
           </a>
-          {/* <Button bgColor="translucent" hover rounded className="!h-11">
-            <FaSearch />
-          </Button> */}
         </div>
       </div>
     </div>
