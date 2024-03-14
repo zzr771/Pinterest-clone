7335a177f646cac9aae1c9fc60f6a103379b5bec
Ray <771446555@qq.com>
2024-03-15 00:40:14 +0800

    component: navbarbottom

--- components/layout/NavBarBottom.tsx
+++ components/layout/NavBarBottom.tsx
@@ -1,17 +1,23 @@
 "use client"
 
 import { useState } from "react"
+import { useRouter, usePathname } from "next/navigation"
 import { FaSearch, FaUser } from "react-icons/fa"
 import { AiFillHome } from "react-icons/ai"
 
 export default function NavBarBottom() {
   if (window.innerWidth >= 820) return null
+
   const [activeBtn, setActiveBtn] = useState("Home")
+  const router = useRouter()
+  const pathname = usePathname()
 
   return (
     <section className="sm:nav-float-bottom max-w3:nav-bottom bg-white">
       <div className="w-full h-full flex justify-around items-center max-w3:p-0">
-        <div className={`${activeBtn === "Home" ? "text-black" : "text-gray-font-3"}`}>
+        <div
+          onClick={() => router.push("/")}
+          className={`${pathname === "Home" ? "text-black" : "text-gray-font-3"}`}>
           <AiFillHome className="w-6 h-6" />
         </div>
 
@@ -19,7 +25,9 @@ export default function NavBarBottom() {
           <FaSearch className="w-6 h-6" />
         </div>
 
-        <div className={`${activeBtn === "Saved" ? "text-black" : "text-gray-font-3"}`}>
+        <div
+          onClick={() => router.push("/userID")}
+          className={`${activeBtn === "Saved" ? "text-black" : "text-gray-font-3"}`}>
           <FaUser className="w-6 h-6" />
         </div>
       </div>
