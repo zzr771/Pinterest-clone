66b4921db6897a6f16348a7648fd914f3c1aa9b2
Ray <771446555@qq.com>
2024-03-25 19:03:00 +0800

    refactor: replace router with Link

--- components/layout/NavBarBottom.tsx
+++ components/layout/NavBarBottom.tsx
@@ -1,35 +1,35 @@
 "use client"
 
-import { useRouter, usePathname } from "next/navigation"
+import { usePathname } from "next/navigation"
 import { FaSearch, FaUser } from "react-icons/fa"
 import { AiFillHome } from "react-icons/ai"
+import Link from "next/link"
 
 export default function NavBarBottom() {
   if (window.innerWidth >= 820) return null
 
-  const router = useRouter()
   const pathname = usePathname()
 
   return (
     <section className="sm:nav-float-bottom max-w3:nav-bottom bg-white">
       <div className="w-full h-full flex justify-around items-center max-w3:p-0">
-        <div
-          onClick={() => router.push("/")}
-          className={`${pathname === "Home" ? "text-black" : "text-gray-font-3"}`}>
-          <AiFillHome className="w-6 h-6" />
-        </div>
+        <Link href="/">
+          <div className={`${pathname === "Home" ? "text-black" : "text-gray-font-3"}`}>
+            <AiFillHome className="w-6 h-6" />
+          </div>
+        </Link>
 
-        <div
-          onClick={() => router.push("/search")}
-          className={`${pathname === "Search" ? "text-black" : "text-gray-font-3"}`}>
-          <FaSearch className="w-6 h-6" />
-        </div>
+        <Link href="/search">
+          <div className={`${pathname === "Search" ? "text-black" : "text-gray-font-3"}`}>
+            <FaSearch className="w-6 h-6" />
+          </div>
+        </Link>
 
-        <div
-          onClick={() => router.push("/userID")}
-          className={`${pathname === "Saved" ? "text-black" : "text-gray-font-3"}`}>
-          <FaUser className="w-6 h-6" />
-        </div>
+        <Link href="/user/userid">
+          <div className={`${pathname === "Saved" ? "text-black" : "text-gray-font-3"}`}>
+            <FaUser className="w-6 h-6" />
+          </div>
+        </Link>
       </div>
     </section>
   )
