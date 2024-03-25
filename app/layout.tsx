7111330cd7dd8d4cdac50e862b95ea569b9e8bd4
Ray <771446555@qq.com>
2024-03-25 18:57:35 +0800

    feat: Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import "react-slideshow-image/dist/styles.css"
 import dynamic from "next/dynamic"
 const NavBarTop = dynamic(() => import("@/components/layout/NavBarTop"), { ssr: false })
 const NavBarBottom = dynamic(() => import("@/components/layout/NavBarBottom"), { ssr: false })
 import StoreProvider from "@/components/StoreProvider"
 import ScreenReziseMonitor from "@/components/shared/ScreenReziseMonitor"
 import Modal from "@/components/shared/Modal"
+import { ClerkProvider } from "@clerk/nextjs"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -17,16 +19,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <StoreProvider>
-          <ScreenReziseMonitor />
-          <NavBarTop />
-          <NavBarBottom />
-          <Modal />
-          {children}
-        </StoreProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <StoreProvider>
+            <ScreenReziseMonitor />
+            <NavBarTop />
+            <NavBarBottom />
+            <Modal />
+            {children}
+          </StoreProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
