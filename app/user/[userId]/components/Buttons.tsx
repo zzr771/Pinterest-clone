d0db244c0b295e331bbd650115880ef9536b4b6a
Ray <771446555@qq.com>
2024-03-25 19:00:03 +0800

    refactor: rename route path

--- app/user/[userId]/components/Buttons.tsx
+++ app/user/[userId]/components/Buttons.tsx
@@ -3,24 +3,22 @@
 import { useRef, useState } from "react"
 import { TfiMoreAlt } from "react-icons/tfi"
 import Button from "@/components/shared/Button"
-import DropDownList from "@/components/shared/DropDownList"
-import useDropDownList from "@/lib/hooks/useDropDownList"
 import { useRouter } from "next/navigation"
+import dynamic from "next/dynamic"
+const DropDownList = dynamic(() => import("@/components/shared/DropDownList"), { ssr: false })
 
 export default function Buttons() {
   const router = useRouter()
 
-  const [isFollowing, setIsFollowing] = useState(false) // Whether the current user is following the user that he/she is viewing
   const isMySelf = useRef(false) // Whether the current user is viewing the profile of himself/herself
+  const [isFollowing, setIsFollowing] = useState(false) // Whether the current user is following the user that he/she is viewing
 
   const options = useRef([{ label: "Block", callback: () => {} }])
-  const dropContainerRef = useRef<HTMLDivElement>(null)
   const [showDropDownList, setShowDropDownList] = useState(false)
-  useDropDownList({ dropContainerRef, showDropDownList, setShowDropDownList })
 
   return (
     <div>
-      <div className="flex mt-2 gap-7">
+      <div className="flex mt-2 gap-7 items-center">
         {!isMySelf.current && (
           <>
             {isFollowing ? (
@@ -28,7 +26,11 @@ export default function Buttons() {
             ) : (
               <Button key={"red"} text="Follow" bgColor="red" clickEffect hover />
             )}
-            <div ref={dropContainerRef} className="flex items-center relative z-[1]">
+            <DropDownList
+              options={options.current}
+              title="Profile options"
+              position={{ offsetY: 55 }}
+              setShowDropDownFromParent={setShowDropDownList}>
               <Button
                 rounded
                 hover
@@ -37,12 +39,7 @@ export default function Buttons() {
                 bgColor={showDropDownList ? "black" : "transparent"}>
                 <TfiMoreAlt className="w-5 h-5" />
               </Button>
-              {showDropDownList && (
-                <div className="horizontal-middle top-[58px]">
-                  <DropDownList title="Profile options" options={options.current} />
-                </div>
-              )}
-            </div>
+            </DropDownList>
           </>
         )}
         {isMySelf.current && (
