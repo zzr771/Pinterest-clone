3f74ff4fbd9df3397dd3fc055419018407ea6e74
Ray <771446555@qq.com>
2024-04-06 03:29:12 +0800

    rafactor: rename file

--- components/shared/SignInMonitor.tsx
+++ components/shared/SignInMonitor.tsx
@@ -3,13 +3,18 @@
 import { createUser } from "@/lib/actions/user.actions"
 import { useUser } from "@clerk/nextjs"
 import { useEffect } from "react"
+import toast from "react-hot-toast"
 
-export default function SignUpMonitor() {
+export default function SignInMonitor() {
   const { isSignedIn, user } = useUser()
   useEffect(() => {
     if (isSignedIn) {
       const { id, imageUrl, username } = user
-      createUser({ id, imageUrl, username: username || "anonymous" })
+      createUser({ id, imageUrl, username: username || "anonymous" }).then((res) => {
+        if (res?.errorMessage) {
+          toast.error(res.errorMessage)
+        }
+      })
     }
   }, [isSignedIn])
 
