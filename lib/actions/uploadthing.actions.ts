ca28c61597557f486a1ba008716d0db4579cab05
Ray <771446555@qq.com>
2024-04-14 02:42:09 +0800

    feat: delete files in uploadthing

---
+++ lib/actions/uploadthing.actions.ts
@@ -0,0 +1,27 @@
+"use server"
+import { UTApi } from "uploadthing/server"
+import { RequestError } from "../types"
+import { getErrorMessage } from "../utils"
+
+const utapi = new UTApi()
+export async function deleteFiles(arr: string[]): Promise<void | RequestError> {
+  const keys = arr.map((url) => url.split("/").pop() || "")
+  if (keys.length !== arr.length) {
+    return {
+      errorMessage: "Invalid Image URL",
+    }
+  }
+
+  try {
+    const deleteResult = await utapi.deleteFiles(keys)
+    if (!deleteResult.success) {
+      return {
+        errorMessage: "Failed to delete file",
+      }
+    }
+  } catch (error) {
+    return {
+      errorMessage: getErrorMessage(error),
+    }
+  }
+}
